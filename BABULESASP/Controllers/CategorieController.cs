8fdc13930930a53f0dd968b3ec42aacaffc8b6af
Yaroslav-Ludchak1234 <yaroslav.ludchak-pi1901@kep.nung.edu.ua>
2023-04-05 16:18:45 +0300

    add changed models and controller

---
+++ BABULESASP/Controllers/CategorieController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BABULESASP.Models;
+
+namespace BABULESASP.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private FinanceManagerDBEntities db = new FinanceManagerDBEntities();
+
+        // GET: Categorie
+        public ActionResult Index()
+        {
+            return View(db.tblCategories.ToList());
+        }
+
+        // GET: Categorie/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCategorie tblCategorie = db.tblCategories.Find(id);
+            if (tblCategorie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategorie);
+        }
+
+        // GET: Categorie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorie/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ct_name,ct_description")] tblCategorie tblCategorie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblCategories.Add(tblCategorie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblCategorie);
+        }
+
+        // GET: Categorie/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCategorie tblCategorie = db.tblCategories.Find(id);
+            if (tblCategorie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategorie);
+        }
+
+        // POST: Categorie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ct_name,ct_description")] tblCategorie tblCategorie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblCategorie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblCategorie);
+        }
+
+        // GET: Categorie/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCategorie tblCategorie = db.tblCategories.Find(id);
+            if (tblCategorie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCategorie);
+        }
+
+        // POST: Categorie/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblCategorie tblCategorie = db.tblCategories.Find(id);
+            db.tblCategories.Remove(tblCategorie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BABULESASP/Controllers/TransactionController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BABULESASP.Models;
+
+namespace BABULESASP.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private FinanceManagerDBEntities db = new FinanceManagerDBEntities();
+
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View(db.tblTransactions.ToList());
+        }
+
+        // GET: Transaction/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblTransaction tblTransaction = db.tblTransactions.Find(id);
+            if (tblTransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblTransaction);
+        }
+
+        // GET: Transaction/Create
+        public ActionResult Create()
+        {
+            List<string> tblategories = (from data in db.tblCategories select data.ct_name.Distinct<>).ToList();
+            ViewBag.CategoriesListforName = new SelectList(tblategories);
+            return View();
+        }
+
+        // POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,tr_categorie,tr_type,tr_sum,tr_date,tr_description_")] tblTransaction tblTransaction)
+        {
+            List<string> tblategories = (from data in db.tblCategories select data.ct_name).ToList();
+            ViewBag.CategoriesListforName = new SelectList(tblategories);
+            if (ModelState.IsValid)
+            {
+                db.tblTransactions.Add(tblTransaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblTransaction);
+        }
+
+        // GET: Transaction/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblTransaction tblTransaction = db.tblTransactions.Find(id);
+            if (tblTransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblTransaction);
+        }
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,tr_categorie,tr_type,tr_sum,tr_date,tr_description_")] tblTransaction tblTransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblTransaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblTransaction);
+        }
+
+        // GET: Transaction/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblTransaction tblTransaction = db.tblTransactions.Find(id);
+            if (tblTransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblTransaction);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblTransaction tblTransaction = db.tblTransactions.Find(id);
+            db.tblTransactions.Remove(tblTransaction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
